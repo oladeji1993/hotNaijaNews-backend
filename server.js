35e377e61ba2bfd96bbdc1c8654e7d6234969406
Oladeji Olanipekun <Oladeji.Olanipekun@Sterling.ng>
2023-10-14 07:55:28 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+// routes 
+
+app.get('/', (req, res) =>{
+    res.send('hello node API')
+})
+
+app.listen(3000, ()=>{
+    console.log('node API app is running on port 3000')
+})
